52138e6701cdc3e268d7aa5300064a34b71fda80
Dylan W <139509928+dw-610@users.noreply.github.com>
2025-07-12 15:47:30 -0500

    refactored load-data.py to be able to reuse load_data function

--- load-data.py
+++ load-data.py
@@ -1,44 +1,52 @@
 """This script loads in and inspect the .wav IQ data."""
 
-# imports
 from  scipy.io import wavfile
 import numpy as np
 import matplotlib.pyplot as plt
 
-dir = 'IQ/2025_07_12/'
-file = '15-26-26_103900000Hz.wav'
 
-# load the .wav file
-fs, data = wavfile.read(dir + file)
-Ts = 1/fs
+def load_data(filepath: str):
+    """Wrapper for loading in data."""
+    fs, data = wavfile.read(filepath)
+    Ts = 1/fs
+    return fs, Ts, data
 
-print(f'\nSampling rate:       {fs/1e6} MHz')
-print(f'Sample duration:     {Ts*1e6} us')
-print(f'Length of recording: {Ts * len(data):.3f} s')
 
-# extract the IQ data
-I, Q = data[:, 0], data[:, 1]
+if __name__=="__main__":
+    
+    dir = 'IQ/2025_07_12/'
+    file = '15-26-26_103900000Hz.wav'
 
-# check the number of unique values - full utilization would be 2^16 = 65536
-# - increase RF Gain slider to improve utilization
-# - decrase RF Gain slider to remedy clipping
-print(f'\nUnique I values: {len(np.unique(I))}')
-print(f'Unique Q values: {len(np.unique(Q))}')
+    # load the .wav file
+    fs, Ts, data = load_data(dir + file)
 
-# plot the first 1000 samples of the data
-t = Ts * np.arange(500) * 1e3  # time axis
-plt.figure()
-plt.plot(t, I[:500], label = 'I')
-plt.plot(t, Q[:500], label = 'Q')
-plt.title('FM Baseband IQ Data')
-plt.xlabel('Time (ms)')
-plt.legend()
+    print(f'\nSampling rate:       {fs/1e6} MHz')
+    print(f'Sample duration:     {Ts*1e6} us')
+    print(f'Length of recording: {Ts * len(data):.3f} s')
 
-# plot a histogram of the I data to see if clipping is taking place
-plt.figure()
-plt.hist(I, bins=512, log=True)
-plt.title('I Component Histogram')
-plt.xlabel('Sample values')
-plt.ylabel('Value counts (log scale)')
+    # extract the IQ data
+    I, Q = data[:, 0], data[:, 1]
 
-plt.show()
+    # check the number of unique values - full utilization would be 2^16 = 65536
+    # - increase RF Gain slider to improve utilization
+    # - decrease RF Gain slider to remedy clipping
+    print(f'\nUnique I values: {len(np.unique(I))}')
+    print(f'Unique Q values: {len(np.unique(Q))}')
+
+    # plot the first 1000 samples of the data
+    t = Ts * np.arange(500) * 1e3  # time axis
+    plt.figure()
+    plt.plot(t, I[:500], label = 'I')
+    plt.plot(t, Q[:500], label = 'Q')
+    plt.title('FM Baseband IQ Data')
+    plt.xlabel('Time (ms)')
+    plt.legend()
+
+    # plot a histogram of the I data to see if clipping is taking place
+    plt.figure()
+    plt.hist(I, bins=512, log=True)
+    plt.title('I Component Histogram')
+    plt.xlabel('Sample values')
+    plt.ylabel('Value counts (log scale)')
+
+    plt.show()
